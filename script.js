82bac1c8033d51c581c10ff855c94a61b45b11b2
DarielOwl <darieldesosa@gmail.com>
2022-02-13 06:17:00 -0300

    MVC P5, La pelota se mueve

--- script.js
+++ script.js
@@ -11,13 +11,40 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false; //para poner pausa el juego
     }
 
     self.Board.prototype = {
         get elements(){
-            var elements = this.bars;
-            //elements.push(this.ball);
+            var elements = this.bars.map(function(bar){return bar;});
+            elements.push(this.ball);
             return elements;
+        }
+    }
+
+})();
+
+//Configurar Pelota
+(function(){
+    
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+
+    //Crear metodo para que se mueva la pelota
+    self.Ball.prototype = {
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
         }
     }
 
@@ -71,6 +98,20 @@
                 var el = this.board.elements[i];
                 draw(this.ctx,el); 
             };
+        },
+
+        play: function(){
+            if(this.board.playing){ //Si el juego sale de pausa, reanuda
+                
+                //Limpiar los cuadros que quedan atras
+                this.clean();
+
+                //Dibujar los objetos
+                this.draw();
+
+                //Movimiento de la pelota en el tablero
+                this.board.ball.move();
+            }
         }
     }
 
@@ -78,6 +119,12 @@
         switch(element.kind){
             case "rectangle":
                 ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y,element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
                 break;
         }
     }
@@ -90,11 +137,12 @@ var bar = new Bar(20,100,40,100,board);
 var bar2 = new Bar(740,100,40,100,board);
 var canvas = document.getElementById("canvas");
 var boardView = new BoardView(canvas,board);
+var ball = new Ball(350,100,10,board);
 
 //Configurar el movimiento de las barras
 document.addEventListener("keydown",function(ev){
     
-    ev.preventDefault();
+    //ev.preventDefault();
 
     if(ev.keyCode == 38){ //Flecha arriba
         bar.up();
@@ -108,7 +156,13 @@ document.addEventListener("keydown",function(ev){
     else if(ev.keyCode == 83){ //S
         bar2.down();
     }
+    else if(ev.keyCode == 32){
+        ev.preventDefault();
+        board.playing = !board.playing;
+    }
 });
+
+boardView.draw();
 
 //Animacion para que se mueva las barras
 window.requestAnimationFrame(controller);
@@ -116,11 +170,8 @@ window.requestAnimationFrame(controller);
 //Funcion main
 function controller(){ //Controlador
 
-    //Limpiar los cuadros que quedan atras
-    boardView.clean();
-
-    //Dibujar los objetos
-    boardView.draw();
+    //Start the Game
+    boardView.play();
 
     //Animacion para que se mueva las barras
     window.requestAnimationFrame(controller);
